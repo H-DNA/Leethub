2622a5e2b09d3b5aa117c2ac9ccb2d341fc196b7
Đỗ Nguyễn An Huy <huydo862003@gmail.com>
2024-11-21 02:12:49 +0700

    Time: 30 ms (13.33%), Space: 7 MB (95%) - LeetHub

---
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.go
@@ -0,0 +1,18 @@
+func numberOfBeams(bank []string) int {
+    res, prev := 0, 0;
+    
+    for _, str := range bank {
+        cur := 0;
+        for _, c := range str {
+            if c == '1' {
+                cur += 1;
+            }
+        }
+        if cur > 0 {
+            res += prev * cur;
+            prev = cur;
+        }
+    }
+
+    return res;
+}
